081e6bb102d3c841d012786fcccc30022bc18c3a
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-29 22:04:24 -0600

    Adding the exec of class1_2 and class1

---
+++ class1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  // Directiva de procesamiento, cargar biblioteca iostream
+using namespace std; // Espacio de trabajo.
+int main()
+{ // Funcion principal
+    // Declaramos variables
+    int numero;
+    string palabra;
+    cout << "Dame un numero: ";
+    cin >> numero;
+    cout << "Dame una palabra: ";
+    cin >> palabra;
+    cout << "El numero es: " << numero << "La palabra es: "
+         << palabra << endl;
+    return 0;
+}
---
+++ class1_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>  // Directiva de procesamiento, cargar biblioteca iostream
+using namespace std; // Espacio de trabajo.
+int main()
+{
+    int a, x, p = 3;
+    float flotante;
+    char caracter;
+    string palabra;
+    bool boleano;
+    a = 3;
+    cout << "a es igual a: " << a << "p es igual a: " << p << endl;
+    flotante = 3.14;
+    cout << flotante << endl;
+    // Caracter
+    caracter = 'B';
+    cout << caracter << endl;
+    // String
+    palabra = "burro";
+    cout << palabra << endl;
+
+    // Booleano
+    boleano = true;
+    cout << boleano << endl;
+
+    return 0;
+}
