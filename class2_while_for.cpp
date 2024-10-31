69425d8a38499b02a772c9d7f8c7ea10101e794a
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-31 11:16:48 -0600

    Adding Do While

--- class2_while_for.cpp
+++ class2_while_for.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main()
+
+// WHILE
+/* int main()
 {
     int i = 0;
     while (i < 20)
     {
         cout << ++i << endl;
+        if (i == 4)
+        {
+            break; TERMINAR EL CICLO
+        }
+    }
+    return 0;
+} */
+
+// FOR
+/* int main()
+{
+    for (int i = 0; i < 10; i += 2)
+    {
+        cout << i << endl;
+    }
+    return 0;
+} */
+
+// Do while
+int main()
+{
+    int a = 20;
+    do
+    {
+        cout << a << endl;
+        a++;
+    } while ((a >= 21) && (a < 30));
+
+    cout << "Con while" << endl;
+    a = 20;
+    while ((a >= 21) && (a < 30))
+    {
+        cout << a << endl;
+        a++;
     }
     return 0;
 }
