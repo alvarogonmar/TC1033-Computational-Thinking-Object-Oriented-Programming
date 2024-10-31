defcdc9617eec4f09b9ed4c3b10926dbf540d7b5
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-31 13:22:34 -0600

    1hola_mundo finished and 2 added

---
+++ Act_1_c++/1hola_mundo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Directiva de procesamiento, cargar biblioteca iostream
+using namespace std; // Espacio de trabajo.
+
+int main() // Funcion principal
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
