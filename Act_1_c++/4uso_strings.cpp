9801fcedd90618a37fa6307f9b1f614e81753bd1
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-31 18:53:19 -0600

    4 finished

---
+++ Act_1_c++/4uso_strings.cpp
@@ -0,0 +1,14 @@
+// Problema 4 aplicacion que utilice variables tipo string
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string minombre;
+    cout << "Introduce tu nombre: ";
+    cin >> minombre;
+
+    cout << "Hola " << minombre << " bienvenido al mundo de c++";
+    return 0;
+}
