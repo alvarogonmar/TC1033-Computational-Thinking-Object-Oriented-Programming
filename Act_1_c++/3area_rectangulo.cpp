32d55083096b2b9b53171de8e98aba4a509521a1
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-31 18:44:53 -0600

    3 finished

---
+++ Act_1_c++/3area_rectangulo.cpp
@@ -0,0 +1,11 @@
+// Aplicacion que cacula el area de un rectangulo
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float area, base = 5, altura = 10;
+    area = base * altura;
+    cout << "El area del rectangulo con las medidas de: " << base << " de base y " << altura << " de altura es igual a: " << area << endl;
+    return 0;
+}
