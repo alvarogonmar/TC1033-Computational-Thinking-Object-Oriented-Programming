61036b51e5ac88bd3e26a7193a067959a69e21a6
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-31 19:10:02 -0600

    8 finished

---
+++ Act_1_c++/8promedio_calificaciones.cpp
@@ -0,0 +1,24 @@
+// Problema 8 Promedio calificaciones
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    float c1, c2, c3, promedio;
+    string alumno;
+
+    cout << "Introduce el nombre del alumno: ";
+    cin >> alumno;
+
+    cout << "Introduce la primera calificacion: ";
+    cin >> c1;
+    cout << "Introduce la segunda calificacion: ";
+    cin >> c2;
+    cout << "Introduce la tercera calificacion: ";
+    cin >> c3;
+
+    promedio = (c1 + c2 + c3) / 3;
+    cout << "El promedio de: " << alumno << " es: " << promedio << endl;
+    return 0;
+}
