e918cb636dea0fa1e4054a6e616a4bfafa45bfcf
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-31 18:31:47 -0600

    2 finished

---
+++ Act_1_c++/2declara_y_asigna.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  // Directiva de procesamiento, cargar biblioteca iostream
+using namespace std; // Espacio de trabajo.
+
+int main() // Funcion principal
+{
+    int n1 = 2, n2 = 4, suma;
+    suma = n1 + n2;
+    cout << "La suma de n1 y n2 es: " << suma << endl;
+    return 0;
+}
