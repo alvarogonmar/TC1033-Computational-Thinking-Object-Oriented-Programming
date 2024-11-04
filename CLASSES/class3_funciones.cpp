6c110013f72923593f9f013230568a99ffe4e7d3
alvarogonmar <alvarogonmar0@gmail.com>
2024-11-04 10:22:52 -0600

    Adding int suma

--- CLASSES/class3_funciones.cpp
+++ CLASSES/class3_funciones.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
 
-// Caso 1. Entradas y salidas
+// Declaracion de funcion
+int Suma(int, int);
 
+// Caso 1. Entradas y salidas
 int main()
 {
     int a, b;
+    cout << "Dame un valor para a: ";
+    cin >> a;
+    cout << "Dame un valor para b: ";
+    cin >> b;
+    int resultado = Suma(a, b); // Llamar la funcion
+    return 0;
+}
+int Suma(int a, int b)
+{
+    int c;
+    c = a + b;
 }
