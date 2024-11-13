2bc949521a63f93b03ccf55bca78ad70b260d2a6
alvarogonmar <alvarogonmar0@gmail.com>
2024-11-12 20:57:16 -0600

    Problem 1 finished

--- Act3_datos estructurados_arreglos /act3.cpp
+++ Act3_datos estructurados_arreglos /act3.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 int problema_1()
 {
-
+    int arrayy[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Valor: " << arrayy[i] << " Indice " << i << endl;
+    }
     return 0;
 }
 
 int main()
 {
-
+    problema_1();
     return 0;
 }
