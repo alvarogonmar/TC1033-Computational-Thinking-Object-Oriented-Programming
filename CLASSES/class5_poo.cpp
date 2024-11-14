d3e9e9f7d4c999bcf4c5235c3d01a177dc0244a9
alvarogonmar <alvarogonmar0@gmail.com>
2024-11-14 09:50:57 -0600

    New object created (Tigre)

--- CLASSES/class5_poo.cpp
+++ CLASSES/class5_poo.cpp
@@ -53,5 +53,10 @@ int main()
     // Instancear la clase. Crear el objeto
     felino Gato = felino(12, 60, 4, 8, "naranja", "retractiles");
     Gato.dormir();
+    Gato.cazar();
+
+    felino Tigre = felino(700, 100, 4, 20, "naranja", "retractile");
+    Tigre.dormir();
+    Tigre.saltar();
     return 0;
 }
